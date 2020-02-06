8804a4e35a592fddacf4e060d20402fb4ceb30c3
shawinke <stijn.hawinkel@ugent.be>
2020-02-06 17:31:05 +0100

    Proper s4 usage

--- R/F_extractData.R
+++ R/F_extractData.R
@@ -1,12 +1,8 @@
 #' Helper function to extract data matrix from phyloseq, expressionset objects etc. Also filers out all zero rows
-#'
 #' @param logTransformGaussian A boolean, should array data be logtransformed
 #' @param data The list of data objects, either matrix, phyloseq or ExpressionSet objects
-#'
+#' @param ... additional arguments for the extractor function
 #' @return the raw data matrices, samples in the rows
-#'
-#' @importFrom phyloseq taxa_are_rows otu_table t
-#' @importFrom Biobase exprs
 #' @export
 #' @examples
 #' data(zhang)
@@ -15,17 +11,37 @@
 extractData = function(data, logTransformGaussian = TRUE){
     datNames = names(data)
     data = lapply(seq_along(data), function(View){
-        dat  = data[[View]]
-        if(is(dat, "phyloseq") | is(dat, "otu_table")){
-    if(taxa_are_rows(dat)){dat = t(dat)}
-    dat = as(otu_table(dat), "matrix")
-    } else if(is(dat, "ExpressionSet")){
-    dat = t(exprs(dat))
-    if(logTransformGaussian) dat = log(dat)
-    }
-        if(is.null(colnames(dat))) colnames(dat) = paste0("feat", seq_len(ncol(dat)), "View", View)
+        dat  = extractMat(data[[View]],
+                          logTransformGaussian = logTransformGaussian)
+        if(is.null(colnames(dat))){
+            colnames(dat) = paste0("feat", seq_len(ncol(dat)), "View", View)
+        }
         return(dat)
     })
     names(data) = datNames
     data
 }
+#' A function to extract a data matrix from a number of objects
+#' @param Y a phyloseq or eSet object, or another object, or a raw data matrix
+#' @return A data matrix with samples in the rows and features in the columns
+#' @rdname extractMat
+setGeneric("extractMat", function(Y, ...) standardGeneric("extractMat"))
+#'@import methods
+#'@importFrom phyloseq t otu_table taxa_are_rows
+#'@rdname extractMat
+setMethod("extractMat", "phyloseq", function(Y,...){
+    if(taxa_are_rows(Y)) t(as(otu_table(Y), "matrix")) else
+        as(otu_table(Y), "matrix")
+    })
+#'@rdname extractMat
+#'@import methods
+#'@importFrom Biobase exprs
+setMethod("extractMat", "ExpressionSet", function(Y, logTransformGaussian,...){
+    dat = t(exprs(Y))
+    if(logTransformGaussian) dat = log(dat)
+    return(dat)
+})
+#' @rdname extractMat
+setMethod("extractMat", "matrix", function(Y, ...){
+    Y
+})
