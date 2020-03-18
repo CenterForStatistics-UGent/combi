117e3680a3ab26212b4774c6f257df7039fd67a7
shawinke <stijn.hawinkel@ugent.be>
2020-03-18 10:44:19 +0100

    With summary function'

---
+++ R/F_summary.combi.R
@@ -0,0 +1,36 @@
+#' Show an overview of a fitted combi object
+#' @param comb a fitted combi object
+#'
+#' @return A ggplot object containing the plot
+#' @method summary combi
+#'
+#' @export
+#' @examples
+#' data(Zhang)
+#' \dontrun{
+#' #Unconstrained
+#' microMetaboInt = combi(
+#' list("microbiome" = zhangMicrobio, "metabolomics" = zhangMetabo),
+#' distributions = c("quasi", "gaussian"), compositional = c(TRUE, FALSE),
+#' logTransformGaussian = FALSE, verbose = TRUE)
+#' #Constrained
+#' microMetaboIntConstr = combi(
+#'     list("microbiome" = zhangMicrobio, "metabolomics" = zhangMetabo),
+#'     distributions = c("quasi", "gaussian"), compositional = c(TRUE, FALSE),
+#'     logTransformGaussian = FALSE, covariates = zhangMetavars, verbose = TRUE)}
+#'     #Load the fits
+#' load(system.file("extdata", "zhangFits.RData", package = "combi"))
+#' summary(microMetaboInt)
+#' summary(microMetaboIntConstr)
+summary.combi = function(comb){
+    constr = if(is.null(comb$covariates)) "Unconstrained" else "Constrained"
+    dim = length(comb$iter)
+    datSets = length(comb$data)
+    nSam = nrow(comb$latentVars)
+    viewsString = sapply(seq_along(comb$data), function(i){
+        paste0(names(comb$data[i]),": ", ncol(comb$paramEsts[[i]]), "\n")
+    })
+    cat(constr, "combi ordination of", dim, "dimensions on",
+        datSets, "views with", nSam,
+        "samples.\nViews and number of features were:\n", viewsString)
+}
